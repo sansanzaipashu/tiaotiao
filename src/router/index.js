c9b6a3edfc11923b7144a24fb39fd25dfa846ead
ms <599976864@qq.com>
2019-11-01 09:38:28 +0800

    主页

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,64 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Index from '@/views/Index';
+import Home from '@/views/Home';
+import My from '@/views/My';
+import Message from '@/views/Message';
+import Typ from '@/views/Typ';
+import Book from '@/views/Book';
+import Detail from '@/views/Detail';
 
 Vue.use(Router)
+
 
 export default new Router({
   routes: [
     {
       path: '/',
+      redirect:'/index',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path:'/index',
+      redirect:'/home',
+      name:'index',
+      component:Index,
+      children:[
+        {
+          path:'/home',
+          name:'home',
+          component:Home,
+         
+        },
+        {
+          path:'/my',
+          name:'my',
+          component:My
+        },
+        {
+          path:'/message',
+          name:'message',
+          component:Message
+        },
+        {
+          path:'/typ',
+          name:'typ',
+          component:Typ
+        },
+      ]
+    },
+    {
+      path:'/book',
+      name:'book',
+      component:Book
+    },
+    {
+      path:'/detail/:id',
+      name:'detail',
+      component:Detail
     }
+
   ]
 })
---
+++ src/views/Index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div id="container">
+      <router-view></router-view>
+    </div>
+    
+    <van-tabbar v-model="active" :fixed="true" active-color="palevioletred" >
+      <van-tabbar-item icon="home-o" to="/home"></van-tabbar-item>
+      <van-tabbar-item icon="ellipsis" to="/typ"></van-tabbar-item>
+      <van-tabbar-item icon="comment-o" to="/message"></van-tabbar-item>
+      <van-tabbar-item icon="contact" to="/my"></van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Index',
+    data(){
+      return{
+       active: 0
+      }
+    }
+    
+
+}
+</script>
+
+<style scoped="">
+</style>
